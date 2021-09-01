3f11d961d7df04626746fb370b7e49de6795c986
Kent Zhang <kntzhang@gmail.com>
2021-09-01 17:28:22 -0400

    extended details

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ async function getWeatherByLocation(city){
 
 function addWeatherToPage(data){
     const temp = KtoF(data.main.temp);
+    const currentDate = convTime(data.dt);
+    const sunrise = convTime(data.sys.sunrise);
+    const sunset = convTime(data.sys.sunset);
     const weather = document.createElement('div');
     weather.classList.add('weather');
 
@@ -26,11 +29,26 @@ function addWeatherToPage(data){
         <h2><img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png"/>
         ${temp}°F
         </h2>
-        <small>${data.weather[0].main}</small>
+        <small>${data.weather[0].main} - ${data.weather[0].description}</small>
+        <p>Last Updated: ${currentDate}</p>
+        <p>Sunrise: ${sunrise}</p>
+        <p>Sunset: ${sunset}</p>
     `;
 
     main.innerHTML = '';
     main.appendChild(weather);
+}
+
+function convTime(T){
+    const myDate = new Date(T*1000);
+    const hours = myDate.getHours();
+    const minutes = myDate.getMinutes();
+    const seconds = myDate.getSeconds();
+    return formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
 }
 
 function KtoF(K){
